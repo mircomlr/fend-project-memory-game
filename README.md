ed5546926e0669211b69404e1f28edd4fa64da63
Mirco Mueller <mirco.mueller@posteo.de>
2018-04-16 18:37:04 +0200

    Post-processed MVP

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Memory Game Project
+# Memory Game Project (Udacity FEND) by Mirco Müller
 
 ## Table of Contents
 
@@ -7,14 +7,15 @@
 
 ## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+This starter project had some HTML and CSS styling to display a static version of the Memory Game project. I converted this project from a static project to an interactive one. This required modifying the HTML file, but primarily the JavaScript file.
 
-To get started, open `js/app.js` and start building out the app's functionality
+I started by opening `js/app.js` and started building out the app's functionality.
 
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+The game works like a usual memory game. Bootstrap and Google fonts are used (https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css / https://fonts.googleapis.com/css?family=Coda).
+The game can be started by the restarter icon or by refreshing the site. If you need less than 25 moves you will stay on 3 stars, till 49 there will be a 2 star rating.
+
+For specific, detailed instructions of this here solved task , look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
 
 ## Contributing
 
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
-
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+This repository is my solution for the Udacity FEND-Course. Therefore, I most likely will not accept pull requests.
--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 let newList = [];
 let openCardsList = [];
 let jsmoves = [];
+let matchedGameFields = [];
+
 /*
  * Create a list that holds all of your cards
  */
@@ -13,18 +15,18 @@ let list = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube",
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
-newList = shuffle(list);
+newList = shuffle(list); /* shuffle the list... */
 
 let deck = document.getElementsByClassName("deck")[0]
 deck.innerHTML = "";
 
-for (let j = 0; j <16 ; j++) {
-    let para = document.createElement("li");
+for (let j = 0; j <16 ; j++) {                                                  /* loop */
+    let para = document.createElement("li");                                    /* create HTML */
     para.classList.add("card");
     let para2 = document.createElement("i");
     para2.classList.add("fa");
     para2.classList.add(newList[j]);
-    para.appendChild(para2);
+    para.appendChild(para2);                                                    /* add HTML to page */
     deck.appendChild(para);
 };
 
@@ -53,82 +55,105 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
-for (let k =0; k < 16; k++){
+for (let k =0; k < 16; k++){                                                    /* event listener for all cards */
 let card = document.getElementsByClassName("card")[k]
-card.addEventListener('click', showCard)
-card.addEventListener('click', listing)
-card.addEventListener('click', moveCounter)
-card.addEventListener('click', gameCounter)
+card.addEventListener('click', listing);
+card.addEventListener('click', showCard);
+card.addEventListener('click', moveCounter);
 };
 
-function showCard(event) {
-    event.target.classList.add('show');
-};
-
-function listing(event) {
+function listing(event) {                                                       /* open cards */
     openCardsList.push(event.target.firstChild);
     event.target.classList.add('open');
-    clickTime = new Date().getTime();
+    openCardsList[0].parentNode.removeEventListener('click', listing);
+};
+
+function showCard(event) {                                                      /* show cards */
+    event.target.classList.add('show');
+    openCardsList[0].parentNode.removeEventListener('click', showCard)
+};
+
+function moveCounter(event) {                                                   /* count moves */
+    jsmoves.push(event.target.firstChild);
+    let moves = document.getElementsByClassName("moves")[0]
+    moves.innerHTML = jsmoves.length
+    if (jsmoves.length == 25){                                                  /* reduce 1 star after 25 moves*/
+      let stars1 = document.getElementsByClassName("star1")[0];
+      stars1.remove();
+    }
+    else if (jsmoves.length == 50){                                             /* reduce second star after 50 moves*/
+      let stars2 = document.getElementsByClassName("star2")[0];
+      stars2.remove();
+    };
+    openCardsList[0].parentNode.removeEventListener('click', moveCounter);
     matchFunction();
 };
 
-function matchFunction() {
+function matchFunction() {                                                      /* the logic happens from here on (-: */
     if (openCardsList.length == 2)
-    { if (openCardsList[0].classList.value == openCardsList[1].classList.value)
+    { for (let l =0; l < 16; l++){
+        let card = document.getElementsByClassName("card")[l]
+        card.removeEventListener('click', showCard)                             /* interrupt listening*/
+        card.removeEventListener('click', listing)
+        card.removeEventListener('click', moveCounter)
+      };
+      if (openCardsList[0].classList.value == openCardsList[1].classList.value)
       {
-      openCardsList[0].parentNode.classList.remove("open");
+      openCardsList[0].parentNode.classList.remove("open");                     /* if two cards match they will get the class "match"*/
       openCardsList[0].parentNode.classList.remove("show");
       openCardsList[0].parentNode.classList.add("match");
       openCardsList[1].parentNode.classList.remove("open");
       openCardsList[1].parentNode.classList.remove("show");
       openCardsList[1].parentNode.classList.add("match");
-      openCardsList[0].parentNode.removeEventListener('click', showCard)
-      openCardsList[1].parentNode.removeEventListener('click', showCard)
-      openCardsList[0].parentNode.removeEventListener('click', listing)
-      openCardsList[1].parentNode.removeEventListener('click', listing)
+
+      matchedGameFields = document.getElementsByClassName("match");
+      if (matchedGameFields.length == 16) {                                     /* if all cards match game ends */
+          clearInterval(myTimer);
+          alert("Congratulations! Do you want to play again (Press reload button.)? The time for you winning the game is " + sec + " sec..Your star-rating is: " + document.getElementsByClassName("fa-star").length);
+      }
+
       openCardsList = [];
+      for (let m =0; m < 16; m++){
+      let card = document.getElementsByClassName("card")[m]
+
+        if (card.classList != "card match") {
+        card.addEventListener('click', listing);                                /* back to listening*/
+        card.addEventListener('click', showCard);
+        card.addEventListener('click', moveCounter);
+
+        };
+      };
       } else if (openCardsList[0].classList.value != openCardsList[1].classList.value)
       {
           setTimeout(function()
           {
-          openCardsList[0].parentNode.classList.remove("open");
+          openCardsList[0].parentNode.classList.remove("open");                 /* if two cards do not match they will be turned back again... */
           openCardsList[0].parentNode.classList.remove("show");
           openCardsList[1].parentNode.classList.remove("open");
           openCardsList[1].parentNode.classList.remove("show");
           openCardsList = [];
-          }, 1000);
+          for (let m =0; m < 16; m++){
+          let card = document.getElementsByClassName("card")[m]
+
+            if (card.classList != "card match") {
+            card.addEventListener('click', listing);                            /* back to listening*/
+            card.addEventListener('click', showCard);
+            card.addEventListener('click', moveCounter);
+
+            };
+          };
+        }, 1000);                                                               /*...after 1 second */
       }
-    }
-};
 
-function moveCounter(event) {
-    jsmoves.push(event.target.firstChild);
-    let moves = document.getElementsByClassName("moves")[0]
-    moves.innerHTML = jsmoves.length
-    if (jsmoves.length == 25){
-      let stars1 = document.getElementsByClassName("star1")[0];
-      stars1.remove();
     }
-    else if (jsmoves.length == 50){
-      let stars2 = document.getElementsByClassName("star2")[0];
-      stars2.remove();
-    };
-};
+};                                                                              /* end of most relevant logic */
 
-function gameCounter(event) {
-    let matchedGameFields = document.getElementsByClassName("match")
-    if (matchedGameFields.length == 16) {
-        alert("Congratulations! Do you want to play again (Press reload button.)? The time since you startet the game is shown upon the card-game (behind this popup), the stars (max 3) are shown there as well. You finished the game in " + jsmoves.length + " Moves!");
-    }
+function time() {                                                               /* time logic and restart logic from here till end of code */
+sec++;
+document.getElementById("seconds").innerHTML=sec+" sec.";
 };
-
-/* Time-Logic copied from stackoverflow */
 let sec = 0;
-    function pad ( val ) { return val > 9 ? val : "0" + val; }
-    setInterval( function(){
-        document.getElementById("seconds").innerHTML=pad(++sec%60);
-        document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
-    }, 1000);
+let myTimer = setInterval('time()',1000);
 
 let restarter = document.getElementsByClassName("restart")[0]
 restarter.addEventListener('click', restart)
